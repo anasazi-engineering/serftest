b55de52aef7d921025951d230aaea0545f2058f8
Mike M <mike@anasaziengineering.com>
2025-12-05 10:16:01 -0700

    cleaned up

--- main.go
+++ main.go
@@ -16,6 +16,8 @@ func main() {
 	nodeName := flag.String("n", "bootbox001", "Name of the Serf agent")
 	nodeType := flag.String("t", "bootbox", "Type of the Serf agent (bootbox or worker)")
 	flag.Parse()
+
+	// Configure cluster
 	c.Config = ClusterConfig{
 		BindPort: *bindPort, // TODO: change to configurable port
 		BindAddr: *bindAddr, // TODO: discover address
@@ -23,18 +25,16 @@ func main() {
 		NodeType: *nodeType, // TODO: from config
 	}
 
-	fmt.Printf("Starting Serf agent with name: %s, type: %s, address: %s:%d\n",
-		c.Config.NodeName, c.Config.NodeType, c.Config.BindAddr, c.Config.BindPort)
-
 	// Start and/or join cluster
 	outputCh := make(chan string, 1)
 	c.init(outputCh)
 	if c.Config.NodeType == "worker" {
 		fmt.Println("Worker node running...waiting for token response")
-		token := <-outputCh // block here on channel
+		token := <-outputCh // Worker blocks until it receives the token
 		fmt.Printf("Worker node received token: %s\n", token)
 	}
 
+	// The End
 	fmt.Println("\nEnd of main(). Press Ctrl+C to stop the agent")
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
